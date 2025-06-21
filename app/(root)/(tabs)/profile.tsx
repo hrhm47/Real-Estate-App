8889657874f0abdddd7b0aa7711388918bd8fdf6
hrhm47 <haseebr470@gmail.com>
2025-06-21 17:40:56 +0300

    add profile tab screen

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,108 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { settings } from "@/constants/data";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { logout } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import React from "react";
+import {
+  Alert,
+  Image,
+  ImageSourcePropType,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const Profile = () => {
-  return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
 }
 
-export default Profile
+const SettingsItems = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProps) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="flex flex-row justify-between items-center py-3"
+  >
+    <View className="flex flex-row items-center gap-3">
+      <Image source={icon} className="size-6" />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
+        {title}
+      </Text>
+    </View>
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+);
+
+const Profile = () => {
+  const { user, refetch } = useGlobalContext();
+  console.log("User", user);
+
+  const handleLogout = async () => {
+    const reuslt = await logout();
+    if (reuslt) {
+      Alert.alert("Success", "You have been logged out successfully.");
+      refetch();
+    } else {
+      Alert.alert("Logout failed", "Please try again later.");
+    }
+  };
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="bg-white pb-32 px-7"
+      >
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+        </View>
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex-col items-center relative mt-5">
+            <Image
+              source={user ? { uri: user?.avatar } : images.avatar}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+            <Text className="text-2xl font-rubik-bold mt-2">{user? user?.name:"Tempo"}</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-col mt-10">
+          <SettingsItems icon={icons.calendar} title="My Bookings" />
+          <SettingsItems icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItems key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          <SettingsItems
+            icon={icons.logout}
+            title="LogOut"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
